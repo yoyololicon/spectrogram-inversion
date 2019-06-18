ddca10ebb63ff521f20489f5166852ca7dfdd4fa
ycy <ya70201@gmail.com>
2019-06-18 17:18:35 +0800

    add SPSI (very slow) and ADMM method.

--- methods.py
+++ methods.py
@@ -70,6 +70,191 @@ def L_BFGS(spec, transform_fn, samples=None, init_x0=None, maxiter=1000, tol=1e-
 
 
 @torch.no_grad()
+def SPSI(spec, **kwargs):
+    device = spec.device
+    dtype = spec.dtype
+    internal_dict = {'win_length': None, 'window': None, 'hop_length': None, 'center': True, 'normalized': False,
+                     'onesided': True}
+    for key, item in internal_dict.items():
+        try:
+            internal_dict[key] = kwargs[key]
+        except:
+            pass
+    win_length, window, hop_length, center, normalized, onesided = tuple(internal_dict.values())
+
+    if onesided:
+        n_fft = (spec.shape[0] - 1) * 2
+    else:
+        n_fft = spec.shape[0]
+
+    if not win_length:
+        win_length = n_fft
+
+    if not hop_length:
+        hop_length = n_fft // 4
+
+    if window is None:
+        coeff = hop_length / win_length
+    else:
+        coeff = hop_length / window.pow(2).sum()
+
+    offset = (n_fft - win_length) // 2
+    conv_weight = torch.eye(win_length, dtype=dtype, device=device).unsqueeze(1)
+
+    phase = torch.zeros_like(spec)
+
+    def peak_picking(x):
+        mask = (x[1:-1] > x[2:]) & (x[1:-1] > x[:-2])
+        return F.pad(mask, (0, 0, 1, 1))
+
+    L, N = spec.shape
+    for m in tqdm(range(N)):
+        trough = 0
+        for j in range(1, L - 1):
+            if spec[j, m] > spec[j - 1, m] and spec[j, m] > spec[j + 1, m]:
+                a, b, r = spec[j - 1:j + 2, m]
+                p = 0.5 * (a - r) / (a - 2 * b + r)
+                omega = pi2 * (j + p) / n_fft
+                if m:
+                    phase[j, m] = phase[j, m - 1] + hop_length * omega
+                else:
+                    phase[j, m] = hop_length * omega
+
+                if p > 0:
+                    rphase = omega
+                    lphase = omega + np.pi
+                else:
+                    lphase = omega
+                    rphase = omega + np.pi
+
+                j = int(j + 0.5 + p)
+                phase[trough:j, m] = lphase
+                phase[j, m] = rphase
+                while spec[j + 1, m] < spec[j, m] and j < L - 2:
+                    j += 1
+                    phase[j, m] = rphase
+                trough = j + 1
+
+    x = torch.stack((torch.cos(phase), torch.sin(phase)), 2) * spec.unsqueeze(2)
+    x = torch.irfft(x.transpose(0, 1), 1, normalized=normalized, onesided=onesided,
+                    signal_sizes=[n_fft] if onesided else None)[:, offset:offset + win_length] * coeff
+    if window is not None:
+        x *= window
+    x = x.t().unsqueeze(0)
+    x = F.conv_transpose1d(x, conv_weight, stride=hop_length).view(-1)
+    if center:
+        x = x[win_length // 2:-win_length // 2]
+
+    return x
+
+
+@torch.no_grad()
+def ADMM(spec, maxiter=1000, tol=1e-6, rho=0.1, verbose=1, evaiter=10, **kwargs):
+    """
+    Paper: https://pdfs.semanticscholar.org/14bc/876fae55faf5669beb01667a4f3bd324a4f1.pdf
+
+    Parameters
+    ----------
+    spec
+    maxiter
+    tol
+    verbose
+    evaiter
+    hop_length
+    win_length
+    window
+    center
+    pad_mode
+    normalized
+    onesided
+
+    Returns
+    -------
+
+    """
+    device = spec.device
+    dtype = spec.dtype
+    internal_dict = {'win_length': None, 'window': None, 'hop_length': None, 'center': True, 'normalized': False,
+                     'onesided': True}
+    for key, item in internal_dict.items():
+        try:
+            internal_dict[key] = kwargs[key]
+        except:
+            pass
+    win_length, window, hop_length, center, normalized, onesided = tuple(internal_dict.values())
+
+    if onesided:
+        n_fft = (spec.shape[0] - 1) * 2
+    else:
+        n_fft = spec.shape[0]
+
+    if not win_length:
+        win_length = n_fft
+
+    if not hop_length:
+        hop_length = n_fft // 4
+
+    if window is None:
+        coeff = hop_length / win_length
+    else:
+        coeff = hop_length / window.pow(2).sum()
+
+    offset = (n_fft - win_length) // 2
+    conv_weight = torch.eye(win_length, dtype=dtype, device=device).unsqueeze(1)
+
+    def istft(x):
+        x = torch.irfft(x.transpose(0, 1), 1, normalized=normalized, onesided=onesided,
+                        signal_sizes=[n_fft] if onesided else None)[:, offset:offset + win_length] * coeff
+        if window is not None:
+            x *= window
+        x = x.t().unsqueeze(0)
+        x = F.conv_transpose1d(x, conv_weight, stride=hop_length).view(-1)
+        if center:
+            x = x[win_length // 2:-win_length // 2]
+        return x
+
+    X = torch.stack((spec, torch.zeros_like(spec)), -1)
+    x = istft(X)
+    Z = X.clone()
+    Y = X.clone()
+    U = torch.zeros_like(X)
+
+    criterion = nn.MSELoss()
+    init_loss = None
+    with tqdm(total=maxiter, disable=not verbose) as pbar:
+        for i in range(maxiter):
+            reconstruted = torch.stft(x, n_fft, **kwargs)
+            Z[:] = (rho * Y + reconstruted) / (1 + rho)
+            U += X - Z
+
+            # Pc2
+            X[:] = Z - U
+            mag = X.pow(2).sum(2).sqrt()
+            X *= (spec / F.threshold_(mag, 1e-7, 1e-7)).unsqueeze(-1)
+
+            Y[:] = X + U
+            # Pc1
+            x[:] = istft(Y)
+
+            if i % evaiter == evaiter - 1:
+                mag = reconstruted.pow(2).sum(2).sqrt()
+                c = spectral_convergence(mag, spec).item()
+                l2_loss = criterion(mag, spec).item()
+                snr = SNR(mag, spec).item()
+                ser = SER(mag, spec).item()
+                pbar.set_postfix(snr=snr, ser=ser, spectral_convergence=c, loss=l2_loss)
+                pbar.update(evaiter)
+
+                if not init_loss:
+                    init_loss = l2_loss
+                elif (previous_loss - l2_loss) / init_loss < tol * evaiter and previous_loss > l2_loss:
+                    break
+                previous_loss = l2_loss
+
+    return x
+
+
+@torch.no_grad()
 def griffin_lim(spec, maxiter=1000, tol=1e-6, alpha=0.99, verbose=1, evaiter=10, **kwargs):
     """
     Paper: https://pdfs.semanticscholar.org/14bc/876fae55faf5669beb01667a4f3bd324a4f1.pdf
@@ -160,7 +345,7 @@ def griffin_lim(spec, maxiter=1000, tol=1e-6, alpha=0.99, verbose=1, evaiter=10,
 
                 if not init_loss:
                     init_loss = l2_loss
-                elif (previous_loss - l2_loss) / init_loss < tol * evaiter and not alpha:
+                elif (previous_loss - l2_loss) / init_loss < tol * evaiter and previous_loss > l2_loss:
                     break
                 previous_loss = l2_loss
 
@@ -296,11 +481,11 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import numpy as np
 
-    nfft = 2048
-    winsize = 2048
+    nfft = 1024
+    winsize = 1024
     hopsize = 256
 
-    y, sr = librosa.load(librosa.util.example_audio_file(), duration=20)
+    y, sr = librosa.load(librosa.util.example_audio_file())
     # librosa.output.write_wav('origin.wav', y, sr)
     y = torch.Tensor(y).cuda()
     window = torch.hann_window(winsize).cuda()
@@ -327,9 +512,11 @@ if __name__ == '__main__':
     # _, init_x = istft(mag * np.exp(1j * phase), noverlap=1024 - 256)
 
     # estimated = L_BFGS(spec, func, len(y), maxiter=50, lr=1, history_size=10, evaiter=5)
-    # estimated = griffin_lim(spec, maxiter=500, **arg_dict)
+    #estimated = griffin_lim(spec, maxiter=500, alpha=0.95, **arg_dict)
+    #estimated = ADMM(spec, maxiter=500, rho=0.3, tol=0, **arg_dict)
     # arg_dict['hop_length'] = 333
-    estimated = RTISI_LA(spec, maxiter=8, look_ahead=-1, asymmetric_window=True, **arg_dict)
+    estimated = RTISI_LA(spec, maxiter=20, look_ahead=3, asymmetric_window=True, **arg_dict)
+    # estimated = SPSI(spec, **arg_dict)
     estimated_spec = spectrogram(estimated, nfft, **arg_dict)
     display.specshow(librosa.amplitude_to_db(estimated_spec.cpu().numpy(), ref=np.max), y_axis='log')
     plt.show()
