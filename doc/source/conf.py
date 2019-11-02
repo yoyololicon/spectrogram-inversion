10c87573528665c141eef57fca3e0c53ef13f5b1
ycy <ya70201@gmail.com>
2019-11-02 22:20:14 +0800

    add requirements, fix import pytorch theme error on readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import pytorch_sphinx_theme
+#import pytorch_sphinx_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../torch_specinv'))
